09c7933dab9707760cfb194da8957d0b8bc40ea2
wyattlaberge64 <42870908+wyattlaberge64@users.noreply.github.com>
2020-01-13 11:48:07 -0500

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	static int topRange = 9;
+	static int bottomRange = 1;
+	static int correct = 0;
+	static int incorrect = 0;
+	static int question = 0;
+	static boolean stop = false;
+	static boolean again = true;
+	
+	
+}
